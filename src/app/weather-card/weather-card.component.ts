cfc86386c77ae629e134a79e56b81000d56714f6
yaman0 <yamanO@protonmail.com>
2018-04-20 15:54:03 +0200

    Add input attribute for id of city

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 import { Town } from '../model/town';
 import { TownService } from '../services/town/town.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -6,23 +6,27 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'weather-card',
   templateUrl: './weather-card.component.html',
-  styleUrls: ['./weather-card.component.css']
+  styleUrls: ['./weather-card.component.css'],
 })
+
 export class WeatherCardComponent implements OnInit {
   loadmode: boolean;
 	private town: Town;
   private townService: TownService;
+  @Input() townId: number;
+
   constructor(private http: HttpClient) {
 	  this.townService = new TownService(http)
   }
 
   ngOnInit() {
+    console.log(this.townId)
     this.refreshWeather()
   }
 
   refreshWeather(){
     this.loadmode=true;
-    this.townService.getTown(1).subscribe(town => {
+    this.townService.getTown(this.townId).subscribe(town => {
       this.town=town;
       this.loadmode=false;
     });
